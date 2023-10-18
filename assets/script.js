80980419ff67927119440d2ec9a23c2ce7426151
nicos02 <135131275+nicos02@users.noreply.github.com>
2023-10-18 14:33:52 +0200

    Add Com

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Cette fonction s'exécute lorsque le document HTML est entièrement chargé et prêt à être manipulé.
+
   // Capturer la soumission du formulaire
-  $('#myForm').submit(function(event) {
-      event.preventDefault(); // Empêcher la soumission par défaut du formulaire
+  $("#myForm").submit(function (event) {
+    // Cette fonction se déclenche lorsque le formulaire est soumis.
 
-      // Récupérer la valeur saisie par l'utilisateur
-      var valeurNumerique = $('#valeur').val();
+    // Empêcher la soumission par défaut du formulaire, ce qui évite que la page soit actualisée.
+    event.preventDefault();
 
-      // Faites quelque chose avec la valeur (dans cet exemple, affichez-la)
-      console.log("La valeur numérique saisie est : " + valeurNumerique);
+    // Récupérer la valeur saisie par l'utilisateur depuis l'élément avec l'ID "valeur"
+    var valeurNumerique = $("#valeur").val();
 
+    // Faites quelque chose avec la valeur (dans cet exemple, affichez-la dans la console)
+    console.log("La valeur numérique saisie est : " + valeurNumerique);
   });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
+  // Cette fonction s'exécute lorsque le document HTML est entièrement chargé et prêt à être manipulé.
+
   // Génère un nombre aléatoire entre 0 et 100
   var randomValue = Math.floor(Math.random() * 101);
 
+  // Affiche le nombre aléatoire généré dans la console
   console.log("Le nombre aléatoire est : " + randomValue);
 
-  $('#myForm').submit(function(event) {
-      event.preventDefault(); // Empêche la soumission par défaut du formulaire
+  // Sélectionne le formulaire avec l'ID "myForm" et définit un gestionnaire pour l'événement de soumission (submit)
+  $("#myForm").submit(function (event) {
+    // Cette fonction se déclenche lorsque le formulaire est soumis.
 
-      // Récupérer la valeur saisie par l'utilisateur
-      var enteredValue = parseInt($('#valeur').val());
+    // Empêche la soumission par défaut du formulaire, ce qui évite que la page soit actualisée.
+    event.preventDefault();
 
-      // Vérifier si la valeur saisie est égale au nombre aléatoire
-      if (enteredValue === randomValue) {
-          alert("Félicitations ! Vous avez trouvé le nombre aléatoire : " + randomValue);
+    // Récupérer la valeur saisie par l'utilisateur depuis l'élément avec l'ID "valeur"
+    var enteredValue = parseInt($("#valeur").val());
+
+    // Vérifier si la valeur saisie par l'utilisateur est égale au nombre aléatoire généré précédemment.
+    if (enteredValue === randomValue) {
+      // Si les valeurs sont égales, cela signifie que l'utilisateur a trouvé le nombre aléatoire.
+
+      // Affiche une alerte félicitant l'utilisateur pour avoir trouvé le nombre aléatoire et affiche le nombre lui-même.
+      alert(
+        "Félicitations ! Vous avez trouvé le nombre aléatoire : " + randomValue
+      );
+    } else {
+      // Si les valeurs ne sont pas égales, cela signifie que l'utilisateur n'a pas encore trouvé le nombre.
+
+      // Affiche un indice pour aider l'utilisateur à se rapprocher du nombre aléatoire.
+      if (enteredValue < randomValue) {
+        // Si la valeur saisie est inférieure au nombre aléatoire, indique que le nombre à trouver est plus grand.
+        alert("Le nombre est plus grand.");
       } else {
-          // Afficher un indice pour aider l'utilisateur à trouver le nombre
-          if (enteredValue < randomValue) {
-              alert("Le nombre est plus grand.");
-          } else {
-              alert("Le nombre est plus petit.");
-          }
+        // Si la valeur saisie est supérieure au nombre aléatoire, indique que le nombre à trouver est plus petit.
+        alert("Le nombre est plus petit.");
       }
+    }
   });
 });
-
-
-
-
-
-
